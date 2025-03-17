1592b8284bf8251a8128ba63ccdf43946c1640d0
abdullahtamim32 <abdullahtamim3262@gmail.com>
2025-03-17 22:48:34 +0600

    fourth commit

--- js/login.js
+++ js/login.js
@@ -5,10 +5,13 @@ document.getElementById("hero-section-banner-btn").addEventListener("click", () 
    if(secondInputFieldValue === 123456){
      // faq logic section
     function faq (){
-
+      //   hero section banish
        let herosec= document.getElementById("hero-sec-main-parent");
        herosec.style.display = "none"
-    }
+      //  faq sec visible
+      let faqSec = document.getElementById("FAQ-MAIN-PARENT");
+       faqSec.style.display = "inline-block"
+   }
     faq()
      
    }else{
